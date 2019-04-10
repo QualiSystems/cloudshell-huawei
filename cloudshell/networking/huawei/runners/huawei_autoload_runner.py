43b796e5484533e61ad870a61668700059798d0a
alexquali <alexey.b@quali.com>
2019-04-10 20:24:48 +0300

    fix autoload runner

--- cloudshell/networking/huawei/runners/huawei_autoload_runner.py
+++ cloudshell/networking/huawei/runners/huawei_autoload_runner.py
@@ -5,22 +5,22 @@ from cloudshell.devices.runners.autoload_runner import AutoloadRunner
 from cloudshell.networking.huawei.flows.huawei_autoload_flow import HuaweiSnmpAutoloadFlow
 
 
-# class HuaweiAutoloadRunner(AutoloadRunner):
-#     def __init__(self, resource_config, logger, snmp_handler):
-#         super(HuaweiAutoloadRunner, self).__init__(resource_config, logger)
-#         self.snmp_handler = snmp_handler
-#
-#     @property
-#     def autoload_flow(self):
-#         return HuaweiSnmpAutoloadFlow(self.snmp_handler, self._logger)
-#
-#
 class HuaweiAutoloadRunner(AutoloadRunner):
     def __init__(self, resource_config, logger, snmp_handler):
-        super(HuaweiAutoloadRunner, self).__init__(resource_config)
-        self._logger = logger
+        super(HuaweiAutoloadRunner, self).__init__(resource_config, logger)
         self.snmp_handler = snmp_handler
 
     @property
     def autoload_flow(self):
         return HuaweiSnmpAutoloadFlow(self.snmp_handler, self._logger)
+
+
+# class HuaweiAutoloadRunner(AutoloadRunner):
+#     def __init__(self, resource_config, logger, snmp_handler):
+#         super(HuaweiAutoloadRunner, self).__init__(resource_config)
+#         self._logger = logger
+#         self.snmp_handler = snmp_handler
+#
+#     @property
+#     def autoload_flow(self):
+#         return HuaweiSnmpAutoloadFlow(self.snmp_handler, self._logger)
